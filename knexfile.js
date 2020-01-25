41f25b599745439bb6b3460ba6f4549b8ebdb8d1
Tara Sherman <taradelin@gmail.com>
2020-01-25 12:32:01 -0500

    Added dev & test environments to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,25 @@
-module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
-    },
-    seeds: { directory: './database/seeds' },
+const sqlite = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations",
   },
-};
+  seeds: {
+    directory: "./data/seeds",
+  },
+}
+
+module.exports = {
+  dev: {
+    ...sqlite,
+    connection: {
+      filename: "./data/dev.db3",
+    },
+  },
+  test: {
+    ...sqlite,
+    connection: {
+      filename: "./data/test.db3",
+    },
+  },
+}
