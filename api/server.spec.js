bd2244872e21bce41bae3183c2000263ad576f77
Tara Sherman <taradelin@gmail.com>
2020-01-26 12:54:34 -0500

    Login fail Jest; Get jokes fail Jest & Insomnia

--- api/server.spec.js
+++ api/server.spec.js
@@ -7,44 +7,75 @@ beforeEach(async () => {
   await db('users').truncate();
 });
 
-test('welcome route', async () => {
-  const res = await supertest(server).get('/')
+describe('add a user, login, get jokes', () => {
+  it('adds a user to the database', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/register')
+      .send({
+        "username": "testuser",
+        "password": "test"
+      })
+    expect(res.status).toBe(201)
+    expect(res.type).toBe('application/json')
+    expect(res.body.username).toBeString()
+  });
+
+  it('should log in and return a token', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/login')
+      .send({
+        username: "testuser",
+        password: "test"
+      })
     expect(res.status).toBe(200)
     expect(res.type).toBe('application/json')
-    expect(res.body.message).toMatch(/Sprint Challenge Authentication/i)
-});
+    expect(response.body.username).toBeString();
+  });
 
-test('add a user to the database', async () => {
-  const res = await supertest(server)
-    .post('/api/auth/register')
-    .send({
-      "username": "test",
-      "password": "test"
-    })
-  expect(res.status).toBe(201)
-  expect(res.type).toBe('application/json')
-  expect(res.body.username).toBeString()
-});
-
-test('login as a test user and return a token', async () => {
-  const res = await supertest(server)
-    .post('/api/auth/login')
-    .send({
-      "username": "test",
-      "password": "test"
-    })
-  expect(res.status).toBe(200)
-  expect(res.type).toBe('application/json')
-  expect(res.body.token).toBeTruthy()
-});
-
-test('GET jokes from the api', () => {
-  const res = supertest(server)
-    .get('/api/jokes')
-    .set(
-      "Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyLCJ1c2VybmFtZSI6InVzZXIyIiwiaWF0IjoxNTc5OTgxMzM3LCJleHAiOjE1ODA4NDUzMzd9.zj9VA2aVKMcOu3TS5x3wRKXWh1-vZ8LFhPNCHfUrATc","message"
-    )
+  it('should get jokes from the api', () => {
+    const res = supertest(server)
+      .get('/api/jokes')
+      // .set(
+      //   "Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyLCJ1c2VybmFtZSI6InVzZXIyIiwiaWF0IjoxNTc5OTgxMzM3LCJleHAiOjE1ODA4NDUzMzd9.zj9VA2aVKMcOu3TS5x3wRKXWh1-vZ8LFhPNCHfUrATc"
+      // )
     expect(res.status).toBe(200)
     expect(res.type).toBe('application/json')
     expect([res.body]).toBeArray()
-})
+  });
+});
+
+
+// test('add a user to the database', async () => {
+//   const res = await supertest(server)
+//     .post('/api/auth/register')
+//     .send({
+//       "username": "testuser",
+//       "password": "test"
+//     })
+//   expect(res.status).toBe(201)
+//   expect(res.type).toBe('application/json')
+//   expect(res.body.username).toBeString()
+// });
+
+// test('login as a test user and return a token', async () => {
+//   const res = await supertest(server)
+//     .post('/api/auth/login')
+//     .send({
+//       username: "testuser",
+//       password: "test"
+//     })
+//   expect(res.status).toBe(200)
+//   expect(res.type).toBe('application/json')
+//   expect(response.body.token).toBeTruthy();
+// });
+
+// test('GET jokes from the api', () => {
+//   const res = supertest(server)
+//     .get('/api/jokes')
+//     .set(
+//       Authorization, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyLCJ1c2VybmFtZSI6InVzZXIyIiwiaWF0IjoxNTc5OTgxMzM3LCJleHAiOjE1ODA4NDUzMzd9.zj9VA2aVKMcOu3TS5x3wRKXWh1-vZ8LFhPNCHfUrATc
+//     )
+//     expect(res.status).toBe(200)
+//     expect(res.type).toBe('application/json')
+//     expect([res.body]).toBeArray()
+// })
